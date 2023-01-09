cc4505bb5ac8c3946f78557fd0210f04bd9c7de0
Harshal Zodape <zodape_harshal@rediffmail.com>
2023-01-09 12:23:28 +0530

    css approach 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import axios from "axios";
+import styles from './App.module.css' ;
+
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const useSemiPersistentState = (key, initialState) => {
@@ -91,14 +93,15 @@ const App = () => {
 
   
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className={styles.container}>
+
+      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-      <hr />
+      
       {stories.isError && <p>Something went wrong ...</p>}
       {stories.isLoading ? (
         <p>Loading ...</p>
@@ -127,7 +130,7 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className={styles.label}>{children}</label>
       &nbsp;
       <input
         id={id}
@@ -135,6 +138,7 @@ const InputWithLabel = ({
         type={type}
         value={value}
         onChange={onInputChange}
+        className={styles.input}
       />
     </>
   );
@@ -148,7 +152,7 @@ const List = ({ list, onRemoveItem }) => (
   </ul>
 );
 const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit}) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className={styles.searchForm}>
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -157,21 +161,21 @@ const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit}) => (
     >
       <strong>Search:</strong>
     </InputWithLabel>
-    <button type="submit" disabled={!searchTerm}>
+    <button type="submit" disabled={!searchTerm} className={`${styles.button} ${styles.buttonLarge}}`}>
       Search
     </button>
   </form>
   );
 const Item = ({ item, onRemoveItem }) => (
-  <li>
-    <span>
+  <li className={styles.item}>
+    <span style={{width:'40%'}}>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button type="button" onClick={() => onRemoveItem(item)} className={`${styles.button} ${styles.buttonSmall}`}>
         Dismiss
       </button>
     </span>
